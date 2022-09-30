77273d1441c90326d7c6c61fdad51c63da28eb49
fedeargfyron <fedemgs15@gmail.com>
2022-09-30 20:58:59 -0300

    Se agregan endpoints de obtención por id, nombre y alta de productos

--- Controllers/productController.js
+++ Controllers/productController.js
@@ -2,6 +2,15 @@ const Product = require('../Models/productSchema');
 
 const getProducts = async () => await Product.find();
 
+const getById = async (id) => await Product.findById(id);
+
+const getByName = async (name) => await Product.findOne({name: name});
+
+const postProduct = async (body) => new Product(body).save();
+
 module.exports = {
-    getProducts
+    getProducts,
+    getById,
+    getByName,
+    postProduct
 }
--- Routes/productRouter.js
+++ Routes/productRouter.js
@@ -1,5 +1,8 @@
 const {
-    getProducts
+    getProducts,
+    getById,
+    getByName,
+    postProduct
 } = require ('../Controllers/productController')
 
 const router = require('express').Router();
@@ -8,5 +11,29 @@ router.get('/', async (req, res) => {
     let products = await getProducts();
     res.status(200).json(products);
 });
+
+router.get('/byId/:id', async (req, res) => {
+    let product = await getById(req.params.id);
+
+    if(!product)
+        return res.status(404).json(product);
+ 
+    res.status(200).json(product);
+})
+
+router.get('/byName/:name', async (req, res) => {
+    console.log(req.params.name)
+    let product = await getByName(req.params.name);
+
+    if(!product)
+        return res.status(404).json(product);
+ 
+    res.status(200).json(product);
+})
+
+router.post('/', async (req, res) => {
+    await postProduct(req.body);
+    res.status(200).json();
+})
 
 module.exports = router;
